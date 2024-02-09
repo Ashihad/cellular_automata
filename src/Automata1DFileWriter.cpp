22190241c25db08dde238b7969297c5198c7d54e
Ashihad <grzegorz.esser@gmail.com>
2024-02-09 01:20:10 +0100

    Added const iterators in Automata1DFileWriter::writeBoard

--- src/Automata1DFileWriter.cpp
+++ src/Automata1DFileWriter.cpp
@@ -12,7 +12,7 @@ Automata1DFileWriter::Automata1DFileWriter(std::shared_ptr<Automata1DModel> ptr,
 
 void Automata1DFileWriter::writeBoard() {
     const Board1DType& board {std::shared_ptr<Automata1DModel>{model_ptr.lock()}->getBoard()}; 
-    for (auto iter = begin(board); iter != end(board); ++iter) {
+    for (auto iter = cbegin(board); iter != cend(board); ++iter) {
         writeStream << ((*iter == '1') ? aliveSymbol : deadSymbol) << ' ';
     }
     writeStream << '\n';
